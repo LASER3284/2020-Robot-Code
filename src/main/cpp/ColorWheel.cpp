a36875b27ef4104d279bd54080ae7f4ac1ca6cf4
jaredp1 <parkerjared02@gmail.com>
2020-03-11 17:25:26 -0500

    Added more functionality to Color Wheel, incomplete

--- src/main/cpp/ColorWheel.cpp
+++ src/main/cpp/ColorWheel.cpp
@@ -16,10 +16,15 @@ using namespace frc;
 ****************************************************************************/
 CColorWheel::CColorWheel()
 {
-    // Initialize member variables.
-
     // Create object pointers.
+    m_pSpinner = new CANSparkMax(nColorWheelMotor, CANSparkMax::MotorType::kBrushless);
+    m_pSensor  = new ColorSensorV3(I2C::kOnboard);
 
+    // Initialize member variables.
+    m_dSetpoint         = 0.0;
+    m_dPositionActual   = 0.0;
+    m_dVelocityActual   = 0.0;
+    m_nState            = eColorStopped;
 }
 
 /****************************************************************************
@@ -30,7 +35,81 @@ CColorWheel::CColorWheel()
 CColorWheel::~CColorWheel()
 {
     // Delete objects.
+    delete m_pSpinner;
+    delete m_pSensor;
 
     // Set objects to nullptrs.
+    m_pSpinner  = nullptr;
+    m_pSensor   = nullptr;
+}
+
+/****************************************************************************
+    Description:	Init - Ran once to initialize the Color Wheel
+    Arguments:		None
+    Returns:		Nothing
+****************************************************************************/
+void CColorWheel::Init()
+{
+    m_dPositionActual = m_pSpinner->GetEncoder().GetPosition();
+    m_dVelocityActual = m_pSpinner->GetEncoder().GetVelocity();
+}
+
+/****************************************************************************
+    Description:	Tick - Ran every 20ms to update the Color Wheel
+    Arguments:		None
+    Returns:		Nothing
+****************************************************************************/
+void CColorWheel::Tick()
+{
+    switch(m_nState)
+    {
+        case eColorStopped :
+            break;
+
+        case eColorVelocity :
+            break;
+
+        case eColorRed :
+            break;
+
+        case eColorYellow :
+            break;
+
+        case eColorBlue :
+            break;
+
+        case eColorGreen :
+            break;
+    }
+}
+
+/****************************************************************************
+    Description:	MoveToColor - Moves to a given color using the state machine
+    Arguments:		TargetColor color
+    Returns:		Nothing
+****************************************************************************/
+void CColorWheel::MoveToColor(int nColor)
+{
+
+}
+
+/****************************************************************************
+    Description:	MoveToSetpoint - Moves to a velocity setpoint
+    Arguments:		double dSetpoint
+    Returns:		Nothing
+****************************************************************************/
+void CColorWheel::MoveToSetpoint(double dSetpoint)
+{
+
+}
+
+/****************************************************************************
+    Description:	IsAtSetpoint - Returns if at the velocity setpoint
+    Arguments:		None
+    Returns:		bool - Is at velocity setpoint
+****************************************************************************/
+bool CColorWheel::IsAtSetpoint()
+{
+
 }
 ///////////////////////////////////////////////////////////////////////////////
--- src/main/include/ColorWheel.h
+++ src/main/include/ColorWheel.h
@@ -7,12 +7,19 @@
 #ifndef ColorWheel_h
 #define ColorWheel_h
 
-#include <frc/I2C.h>
 #include <rev/CANSparkMax.h>
+#include <rev/ColorSensorV3.h>
+#include <IOMap.h>
 
 using namespace frc;
+using namespace rev;
 
-// Default constants for the CColorWheel class.
+// Default constants for the ColorWheel class.
+const double dColorWheelTargetVelocity  =  60.0;
+const double dColorWheelConversionRatio =  142.2;
+
+// ColorWheel enum.
+enum TargetColor {eColorStopped, eColorVelocity, eColorRed, eColorYellow, eColorBlue, eColorGreen};
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -26,11 +33,22 @@ class CColorWheel
 public:
     CColorWheel();
     ~CColorWheel();
+    void Init();
+    void Tick();
+    void MoveToColor(int nColor);
+    void MoveToSetpoint(double dSetpoint);
+    bool IsAtSetpoint();
 
 private:
     // Object Pointers.
+    CANSparkMax*    m_pSpinner;
+    ColorSensorV3*  m_pSensor;
 
     // Member variables.
+    double  m_dSetpoint;
+    double  m_dVelocityActual;
+    double  m_dPositionActual;
+    int     m_nState;
 };
 /////////////////////////////////////////////////////////////////////////////
 #endif
